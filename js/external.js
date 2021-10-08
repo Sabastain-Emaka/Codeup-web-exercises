0e13290f854d2ab323d5654ab6f751e33b148196
sabastain wakoyi <jevian@WAKOYIs-MacBook-Pro.local>
2021-10-08 16:53:28 -0500

    external

--- js/external.js
+++ js/external.js
@@ -3,3 +3,32 @@ console.log("Hello from external JavaScript");
 var alertWelcomeToMyWebsite = alert('Welcome to my Website!');
 
 var promptWhatIsYourFavoriteColor = prompt("what is your favorite color");
+alert("Ccolor "+ " Amazing " + "promptWhatIsYourFavorite " + "is my favorite color too!");
+
+var costPerPay = 3;
+var littleMermaid = prompt( "For how many days do you want to rent little mermaid");
+var brotherBear = prompt( "For how many days do you want to rent brother Bear");
+var hercules = prompt( "For how many days do you want to hercules");
+var totalCost = alert(littleMermaid * 3 + brotherBear * 3 + hercules * 3 + " is your total");
+
+var companies = 3;
+var amazon = prompt( " How many hours did you work at amazon");
+var google = prompt( "How many hours did you work at google");
+var facebook = prompt( " How many hours did you work at facebook");
+var totalSalary = alert(amazon * 380 + google * 400 + facebook * 350 + " is your total salary");
+
+var itemsBought = prompt("Did you buy more than two times");
+alert(" if yes " + "offer was applied " + "if no " + "offer was declined");
+var premiumMember = prompt( "are you a premium member");
+alert (" if yes " + " then buy how you like");
+
+
+
+
+
+
+
+
+
+
+
