1daec03385e243246d5913e47f608700b37b8baa
sabastain wakoyi <jevian@WAKOYIs-MacBook-Pro.local>
2021-10-08 13:52:43 -0500

    js exercises

---
+++ html lecture.js
@@ -0,0 +1,17 @@
+// "use strict";
+
+var jeff = "Jeff"
+console.log(jeff);
+
+console.log("Hello am in my own file now");
+
+var returnOfAlert=alert("TGIF! Thank God I'm Jevian");
+console.log(reurnOfAlert);
+
+var returnOfConfirm = confirm(" Got my weekend");
+console.log(returnOfConfirm);
+
+var whoAreYou = prompt("Who are you?");
+console.log("Jevian" + whoAreYou + "!");
+
+
---
+++ js/external.js
@@ -0,0 +1,5 @@
+console.log("Hello from external JavaScript");
+
+var alertWelcomeToMyWebsite = alert('Welcome to my Website!');
+
+var promptWhatIsYourFavoriteColor = prompt("what is your favorite color");
