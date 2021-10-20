70c897e5220f9dd5b566311d3fea2b10707597ef
sabastain wakoyi <jevian@WAKOYIs-MacBook-Pro.local>
2021-10-20 11:40:25 -0500

    done

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,23 +18,27 @@
      * the step that it describes
      */
     //
-    planets.unshift('The Sun');
+
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift('The Sun');
     console.log(planets);
 
     //
-    planets.push('Pluto')
+
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto')
     console.log(planets);
 
     //
-    planets.shift('The Sun')
+
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift('The Sun')
     console.log(planets);
 
     //
-    planets.pop('Pluto')
+
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop('Pluto')
     console.log(planets);
 
     //
@@ -42,12 +46,14 @@
     console.log(planets.indexOf('Earth'));
 
     //
-    planets.reverse();
+
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     //
-    planets.sort();
+
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 })();
