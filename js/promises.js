f058f5b94b3846a5531ca871f00d5aa509f12421
sabastain wakoyi <jevian@WAKOYIs-MacBook-Pro.local>
2021-12-09 16:55:08 -0600

    promises exercises

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,9 @@
+'use strict';
+
+
 
 function  getLastEventDateByUsername(username) {
-    let GITHUB_API_URL = 'https://api.github.com/users/${username}/events/public'
+    let GITHUB_API_URL = 'https://api.github.com/user/${username}/events/public'
     return fetch(GITHUB_API_URL, {headers: {'Authorization': 'token ${GITHUB_KEY}' }})
         .then(response => response.json()
             .then(listOfEvents => {
@@ -9,7 +12,9 @@ function  getLastEventDateByUsername(username) {
         )
 }
 
-getLastEventDateByUsername('sabastainwakoyi').then(eventDate => console.log(eventDate) )
+getLastEventDateByUsername('sabastainwakoyi').then(eventDate => (eventDate) )
+
+
 
 const wait = milliseconds => {
     return new Promise((resolve, reject ) => {
@@ -19,7 +24,6 @@ const wait = milliseconds => {
     });
 }
  wait(3000).then(ms => console.log('it will show after ${ms/1000} seconds'))
-
 
 
 
@@ -45,3 +49,5 @@ const wait = milliseconds => {
 //         .then(date => console.log(date));
 // };
 // lastCommit("sabastainwakoyi");
+
+
